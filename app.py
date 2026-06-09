29c44cf3e8e0b59666b1cf456b542260fcf818a2
adrianalinarestoledo-creator <adrianalinarestoledo@gmail.com>
2026-06-09 10:25:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-@app.route("/")
-def home():
-    return "SICOF-1 está funcionando correctamente"
+def create_app():
+    app = Flask(__name__)
+
+    # Configuración de la base de datos (Render PostgreSQL)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "TU_URL_DE_RENDER"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = "clave-super-secreta"
+
+    db.init_app(app)
+
+    # Importar modelos
+    from models import Usuario, Folio, DocumentoInterno
+
+    @app.route("/")
+    def home():
+        return "SICOF-1 está funcionando correctamente"
+
+    return app
+
+app = create_app()
